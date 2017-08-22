d1c04c3a79bb84e421ad8ec3751ad222f7b981e1
XxinLiang <alanxxry@vip.qq.com>
2017-08-22 17:37:47 +0800

    Header

--- src/Components/Header/main.jsx
+++ src/Components/Header/main.jsx
@@ -8,20 +8,27 @@ class Header extends Component {
         return (
             <header className="g_hd">
                 <div className="g_ct fix">
-                    <Link to="/" className="logo">
-                        <img src={logoImg} alt="" />
-                    </Link>
-                    <nav className="nav">
-                        <ul className="fix">
-                            <li><Link to="/news">新闻</Link></li>
-                            <li><Link to="/articles">文章</Link></li>
-                            <li><Link to="/videos">视频</Link></li>
-                            <li><Link to="/gtalk">Gtalk</Link></li>
-                            <li><Link to="/platform">平台</Link></li>
-                            <li><Link to="/subject">专题</Link></li>
-                            <li><Link to="/app">App</Link></li>
-                        </ul>
-                    </nav>
+                    <div className="hd_logo fix">
+                        <Link to="/" className="logo">
+                            <img src={logoImg} alt="" />
+                        </Link>
+                    </div>
+                    <div className="hd_nav">
+                        <div className="search">
+                            <input type="text" placeholder="Search"/>
+                        </div>
+                        <nav className="nav">
+                            <ul className="fix">
+                                <li><Link to="/news">新闻</Link></li>
+                                <li><Link to="/articles">文章</Link></li>
+                                <li><Link to="/videos">视频</Link></li>
+                                <li><Link to="/gtalk">Gtalk</Link></li>
+                                <li><Link to="/platform">平台</Link></li>
+                                <li><Link to="/subject">专题</Link></li>
+                                <li><Link to="/app">App</Link></li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </header>
         )
